e629acda832ca2c03ab1b2a4300ce5a85828ffed
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2025-01-19 20:09:15 +0200

    rewrite .css files to .scss, add style.scss

--- storonnimv.client/src/components/elements/group/groupPageComponents/ShortMemberItem.tsx
+++ storonnimv.client/src/components/elements/group/groupPageComponents/ShortMemberItem.tsx
@@ -1,7 +1,7 @@
 ﻿import {Container, Image} from "react-bootstrap";
 import {IMember} from "../../../../models/group/IGroupInfo";
 import {FC} from "react";
-import "../../../../styles/elements/group/GroupMembers.css";
+import "../../../../styles/elements/group/ShortMemberItem.css";
 
 interface IMemberItemProps {
     member: IMember;
