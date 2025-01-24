aedc4aafbcc811fee33b97056dc24e9fc5c0ac95
Sannido <dimabutnik71@gmail.com>
2025-01-24 18:13:56 +0200

    added styles for schedules page

--- storonnimv.client/src/components/elements/schedule/ScheduleListItem.tsx
+++ storonnimv.client/src/components/elements/schedule/ScheduleListItem.tsx
@@ -1,19 +1,21 @@
-﻿import {FC} from "react";
-import {IScheduleListItem} from "../../../models/schedule/IScheduleListItem";
-import {Image} from "react-bootstrap";
+﻿import { FC } from "react";
+import { IScheduleListItem } from "../../../models/schedule/IScheduleListItem";
+import { Image } from "react-bootstrap";
 
 interface ScheduleListItemProps {
     schedule: IScheduleListItem;
 }
 
-export const ScheduleListItem: FC<ScheduleListItemProps> = ({schedule}) => {
+export const ScheduleListItem: FC<ScheduleListItemProps> = ({ schedule }) => {
     return (
         <div className='schedule-list-item'>
             <Image className='schedule-list-item__photo' src={schedule.photo} fluid />
-            <p className='schedule-list-item__title'>{schedule.title}</p>
-            <p className='schedule-list-item__date'>{schedule.performanceDateTime}</p>
-            <p className='schedule-list-item__location'>{schedule.location}</p>
-            <p className='schedule-list-item__status'>{schedule.status}</p>
+            <div className='schedule-list-item__info'>
+                <p className='schedule-list-item__title'>{schedule.title}</p>
+                <p className='schedule-list-item__date'>{schedule.performanceDateTime}</p>
+                <p className='schedule-list-item__location'>{schedule.location}</p>
+                <p className='schedule-list-item__status'>{schedule.status}</p>
+            </div>
         </div>
     );
 };
