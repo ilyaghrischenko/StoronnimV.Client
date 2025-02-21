159c94253bf32b3fd4c964192dd0fe32ba518f79
Lolkash82 <149090430+Lolkash82@users.noreply.github.com>
2025-02-21 23:13:11 +0100

    add editVideo button(not ready)

--- storonnimv.client/src/components/EditsButtons/VideoEditButton.tsx
+++ storonnimv.client/src/components/EditsButtons/VideoEditButton.tsx
@@ -1,1 +1,97 @@
-// TODO : Прописать эту кнопку + решит пред в GroupMemberEditButton
+import React, { useState, useContext, useEffect } from "react";
+import { Button } from "react-bootstrap";
+import { GlobalContext } from "../contexts/shared/GlobalContext"; 
+import { IVideoModel } from "../../models/video/IVideoModel";
+
+interface VideoEditButtonProps {
+    video: IVideoModel;
+    apiUrl: string;
+}
+
+const VideoEditButton: React.FC<VideoEditButtonProps> = ({ video, apiUrl }) => {
+    const { OnShowModal, OnHideModal, sendRequest } = useContext(GlobalContext)!;
+    const [editedVideo, setEditedVideo] = useState<IVideoModel>(video);
+    const [isAuthorized, setIsAuthorized] = useState(false);
+
+    useEffect(() => {
+        const token = sessionStorage.getItem("token");
+        if (token) {
+            setIsAuthorized(true);
+        } else {
+            setIsAuthorized(false);
+        }
+    }, []);
+
+    const handleSave = async () => {
+        try {
+            const response = await sendRequest(`${apiUrl}/${editedVideo.id}`, 'PUT', editedVideo, {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${sessionStorage.getItem("token")}`,
+            });
+
+            if (response.status === 200) {
+                console.log("Збережено: ", editedVideo);
+                OnHideModal();
+            } else {
+                throw new Error("Помилка при збереженні відео");
+            }
+        } catch (error) {
+            console.error("Помилка при збереженні відео:", error);
+        }
+    };
+
+    const handleShowModal = () => {
+        OnShowModal(
+            <div>
+                <label>Назва відео:</label>
+                <input
+                    type="text"
+                    name="title"
+                    value={editedVideo.title}
+                    onChange={(e) => setEditedVideo({ ...editedVideo, title: e.target.value })}
+                    className="form-control"
+                />
+                <label>Опис:</label>
+                <textarea
+                    name="description"
+                    value={editedVideo.description}
+                    onChange={(e) => setEditedVideo({ ...editedVideo, description: e.target.value })}
+                    className="form-control"
+                />
+                <label>Змінити файл відео:</label>
+                <input
+                    type="file"
+                    onChange={(e) => {
+                        const file = e.target.files ? e.target.files[0] : null;
+                        if (file) {
+                            setEditedVideo({
+                                ...editedVideo,
+                                url: URL.createObjectURL(file),
+                            });
+                        }
+                    }}
+                    className="form-control"
+                />
+                <Button onClick={handleSave} className="mt-2">Зберегти</Button>
+            </div>
+        );
+    };
+
+    if (!isAuthorized) {
+        return null;
+    }
+
+    return (
+        <Button
+            className="btn btn-warning position-absolute top-0 end-0 m-2"
+            onClick={handleShowModal}
+            title="Редагувати відео"
+        >
+            ✏
+        </Button>
+    );
+};
+
+export { VideoEditButton };
+
+// TODO : ДОПИСАТЬ ЭТУ КНОПКУ!!!!!
--- storonnimv.client/src/components/elements/video/VideoListItem.tsx
+++ storonnimv.client/src/components/elements/video/VideoListItem.tsx
@@ -1,22 +1,56 @@
-﻿import { FC } from "react";
+﻿import React, { useContext, useState } from "react";
+import { Button } from "react-bootstrap";
 import { IVideoModel } from "../../../models/video/IVideoModel";
+import { GlobalContext } from "../../contexts/shared/GlobalContext";
+import { VideoEditButton } from "../../EditsButtons/VideoEditButton";
 
 interface IVideoListItemProps {
     videoItem: IVideoModel;
 }
 
-const VideoListItem: FC<IVideoListItemProps> = ({ videoItem }) => {
+const VideoListItem: React.FC<IVideoListItemProps> = ({ videoItem }) => {
+    const { OnShowModal } = useContext(GlobalContext)!;
+    const [isAuthorized, setIsAuthorized] = useState(false);
+
+    useState(() => {
+        const token = sessionStorage.getItem("token");
+        if (token) {
+            setIsAuthorized(true);
+        } else {
+            setIsAuthorized(false);
+        }
+    });
+
+    const handleShowEditModal = () => {
+        OnShowModal(
+            <VideoEditButton
+                video={videoItem} 
+                apiUrl="your-api-url" 
+            />,
+            "Редактирование видео"
+        );
+    };
+
     return (
         <div className="video-list-item">
             <h3 className="video-list-item__title">{videoItem.title}</h3>
             <video
                 className="video-list-item__preview"
                 controls
-                preload="auto" // Видео будет загружаться для захвата кадра
+                preload="auto"
             >
                 <source src={videoItem.url} type="video/mp4" />
-                Your browser does not support the video tag.
+                Ваш браузер не поддерживает тег video.
             </video>
+            {isAuthorized && (
+                <Button
+                    className="btn btn-warning position-absolute top-0 end-0 m-2"
+                    onClick={handleShowEditModal}
+                    title="Редактировать видео"
+                >
+                    ✏
+                </Button>
+            )}
         </div>
     );
 };
