6df064aeeac410b891faf10d38fa8e6e0c45a035
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2025-03-26 15:43:18 +0200

    commit

--- storonnimv.client/src/components/elements/admin/ProtectedRoute.tsx
+++ storonnimv.client/src/components/elements/admin/ProtectedRoute.tsx
@@ -1,5 +1,6 @@
-import React, { ReactNode } from "react";
+import React, {ReactNode, useContext, useEffect} from "react";
 import { Navigate } from "react-router-dom";
+import {GlobalContext} from "../../contexts/shared/GlobalContext.tsx";
 
 interface ProtectedRouteProps {
     children: ReactNode;  
@@ -7,10 +8,26 @@ interface ProtectedRouteProps {
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children, requiredRole }) => {
+    const globalContext = useContext(GlobalContext);
+
+    if (!globalContext) {
+        throw new Error("GlobalContext must be used within a GlobalContextProvider");
+    }
+
+    const { fetchIsAdmin, isAdmin } = globalContext;
+
+    useEffect(() => {
+        fetchIsAdmin();
+    }, []);
+
+    if (!isAdmin) {
+        return <Navigate to="/error?statusCode=401&message=Unauthorised" replace />;
+    }
+
     const adminRole = sessionStorage.getItem("role");
 
     if (!adminRole) {
-        return <Navigate to="/error?statusCode=401&message=Unauthorised" replace />
+        return <Navigate to="/error?statusCode=401&message=Unauthorised" replace />;
     }
 
     if (adminRole !== requiredRole) {
