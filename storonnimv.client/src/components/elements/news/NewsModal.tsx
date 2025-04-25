0c6dcce3470a973b78672fde6d8cefc445890b6a
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2025-04-25 18:21:41 +0300

    news modal design

--- storonnimv.client/src/components/elements/news/NewsModal.tsx
+++ storonnimv.client/src/components/elements/news/NewsModal.tsx
@@ -2,7 +2,7 @@
 import {FC, useContext, useEffect} from "react";
 import {GlobalContext} from "../../contexts/shared/GlobalContext.tsx";
 import {NewsContext} from "../../contexts/NewsContext.tsx";
-import {Col, Container, Row, Image, Button} from "react-bootstrap";
+import {Container, Image, Button} from "react-bootstrap";
 import {ModalLoading} from "../shared/ModalLoading.tsx";
 import {NewsDeleteButton} from "../admin/DeleteButtons/NewsDeleteButton.tsx";
 import {EditNewsItemModalContent} from "./forms/EditNewsItemModalContent.tsx";
@@ -35,25 +35,28 @@ const NewsModal: FC<NewsModalProps> = ({newsId}) => {
 
     return (
         <Container className="news-modal">
-            <Row className="mb-3">
-                <Col xs={12} className="text-center">
-                    {newsFullItem?.photo && <Image className="news-modal__photo" src={newsFullItem.photo}/>}
-                    {newsFullItem?.video && (
-                        <video className="news-modal__video" src={newsFullItem.video} controls/>
-                    )}
-                </Col>
-            </Row>
-            <Row className="mb-3">
-                <Col xs={12} className="news-modal__info">
-                    <h1 className="news-modal__info-title">{newsFullItem?.title}</h1>
-                    <p className="news-modal__info-description">{newsFullItem?.description}</p>
-                </Col>
-            </Row>
-            <Row className="mt-3">
-                <Col xs={12} className="news-modal__details">
-                    <p className="news-modal__details-date">{newsFullItem?.date}</p>
-                </Col>
-            </Row>
+            <h1 className="news-modal__title main-text">{newsFullItem?.title}</h1>
+
+            //TODO: отступы сделать от фотки ровные
+            <div>
+                {newsFullItem?.photo && <Image className="news-modal__photo" src={newsFullItem.photo}/>}
+                <p
+                    className="news-modal__description secondary-text"
+                    style={{textAlign: `${newsFullItem?.photo ? 'justify' : 'center'}`}}
+                >
+                    {newsFullItem?.description}
+                </p>
+            </div>
+
+            <div className="news-modal__info">
+                {newsFullItem?.video && (
+                    <video className="news-modal__video" src={newsFullItem.video} controls preload='auto'/>
+                )}
+            </div>
+
+            <div className="news-modal__details">
+                <p className="news-modal__details-date">{newsFullItem?.date}</p>
+            </div>
 
             {newsFullItem && isAdmin && (
                 <Container className="d-flex justify-content-end gap-2">
@@ -66,8 +69,6 @@ const NewsModal: FC<NewsModalProps> = ({newsId}) => {
                     <NewsDeleteButton newsId={newsFullItem.id} apiUrl="https://localhost:44315/api/admin/news"/>
                 </Container>
             )}
-
-
         </Container>
     );
 };
