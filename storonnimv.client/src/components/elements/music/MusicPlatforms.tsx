bcc62ab715e1716f5fc51b9fdc8839ae525132bf
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2025-04-29 17:47:19 +0300

    music platforms no data

--- storonnimv.client/src/components/elements/music/MusicPlatforms.tsx
+++ storonnimv.client/src/components/elements/music/MusicPlatforms.tsx
@@ -4,12 +4,13 @@ import {MusicPlatformItem} from "./MusicPlatformItem";
 import {MusicContext} from "../../contexts/MusicContext";
 import {GlobalContext} from "../../contexts/shared/GlobalContext.tsx";
 import PreloaderTile from "../shared/PreloaderTile.tsx";
+import {NoData} from "../shared/NoData.tsx";
 
 const MusicPlatforms: FC = () => {
     const musicContext = useContext(MusicContext)!;
     const globalContext = useContext(GlobalContext)!;
 
-    const {pageLoading} = globalContext;
+    const {pageLoading, checkIfNoData} = globalContext;
     const {musicPlatforms, fetchMusicPlatforms} = musicContext;
 
     useEffect(() => {
@@ -19,10 +20,11 @@ const MusicPlatforms: FC = () => {
     return (
         <>
             {!pageLoading ?
-                <ListGroup className='music-platforms-container'>
-                    {musicPlatforms.map((item) =>
-                        <MusicPlatformItem item={item} key={item.id}/>)}
-                </ListGroup>
+                (checkIfNoData(() => !musicPlatforms || musicPlatforms.length === 0) ?
+                 <NoData /> : <ListGroup className='music-platforms-container'>
+                        {musicPlatforms.map((item) =>
+                            <MusicPlatformItem item={item} key={item.id}/>)}
+                    </ListGroup>)
                 :
                 <ListGroup className='music-platforms-container'>
                     {Array(3).fill(null).map(() =>
