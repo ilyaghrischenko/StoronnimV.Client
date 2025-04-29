998c806c1cf61e615bfb3bb7d2e0f27f1e6eab79
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2025-04-29 17:29:20 +0300

    no data component

--- storonnimv.client/src/components/elements/shared/NoData.tsx
+++ storonnimv.client/src/components/elements/shared/NoData.tsx
@@ -3,12 +3,13 @@ import {FC} from "react";
 import NoDataImage from '../../../assets/no-data.svg?react';
 
 interface INoDataProps {
+    className?: string;
     message?: string;
 }
 
-const NoData: FC<INoDataProps> = ({message = "Даних немає"}) => {
+const NoData: FC<INoDataProps> = ({className, message = "Даних немає"}) => {
     return (
-        <div className='empty-data-container'>
+        <div className={`${className} empty-data-container`}>
             <NoDataImage className='empty-data-container__image' />
             <h1 className='empty-data-container__text main-text big-shadow'>{message}</h1>
         </div>
