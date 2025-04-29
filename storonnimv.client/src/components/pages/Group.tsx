571ac0f9c660554b3f1fb6bb73274f428e0a7437
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2025-04-29 17:39:11 +0300

    push

--- storonnimv.client/src/components/pages/Group.tsx
+++ storonnimv.client/src/components/pages/Group.tsx
@@ -20,7 +20,6 @@ const Group: FC = () => {
 
     return (
         <GroupContextProvider>
-            {/*TODO: убрать прокрутку страницы и добавить анимацию на прокрутку текста описания*/}
             <Container className='page'>
                 <GroupDescription/>
             </Container>
