448bd138778805b7848f8a1cfedaddbb40f54420
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2025-05-01 00:32:30 +0300

    push

--- storonnimv.client/src/components/elements/shared/Footer.tsx
+++ storonnimv.client/src/components/elements/shared/Footer.tsx
@@ -1,11 +1,23 @@
-﻿import {FC} from "react";
-import {Container} from "react-bootstrap";
+﻿import {FC, useContext} from "react";
+import {Image} from "react-bootstrap";
+
+import groupSocials from '../../../group-socials.json';
+import {GlobalContext} from "../../contexts/shared/GlobalContext.tsx";
 
 const Footer: FC = () => {
+    const globalContext = useContext(GlobalContext)!;
+
+    const {isAdmin, OnShowModal} = globalContext;
+    //TODO
+
     return (
-        <Container className='footer-container'>
-            <h1>Footer</h1>
-        </Container>
+        <div className='footer-container'>
+            {groupSocials.map((social, index) => (
+                <a key={index} href={social.url} target="_blank" rel="noopener noreferrer" className='footer-container__link'>
+                    <Image src={social.photo} className='footer-container__link-photo' />
+                </a>
+            ))}
+        </div>
     );
 };
 
