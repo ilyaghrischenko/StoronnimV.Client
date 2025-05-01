bf96f22e15a4222fd3b0c21940f04ad88e55b67e
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2025-05-01 18:26:31 +0300

    push before admin methods

--- storonnimv.client/src/components/elements/shared/Footer.tsx
+++ storonnimv.client/src/components/elements/shared/Footer.tsx
@@ -1,21 +1,44 @@
-﻿import {FC, useContext} from "react";
-import {Image} from "react-bootstrap";
+﻿import {FC, useContext, useState} from "react";
+import {Button, Image} from "react-bootstrap";
 
-import groupSocials from '../../../group-socials.json';
 import {GlobalContext} from "../../contexts/shared/GlobalContext.tsx";
+import {FaEdit, FaTrash} from "react-icons/fa";
 
 const Footer: FC = () => {
     const globalContext = useContext(GlobalContext)!;
 
     const {isAdmin, OnShowModal} = globalContext;
-    //TODO
+
+    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
     return (
         <div className='footer-container'>
             {groupSocials.map((social, index) => (
-                <a key={index} href={social.url} target="_blank" rel="noopener noreferrer" className='footer-container__link'>
-                    <Image src={social.photo} className='footer-container__link-photo' />
-                </a>
+                <div
+                    key={index}
+                    className='footer-container__item'
+                    onMouseEnter={() => setHoveredIndex(index)}
+                    onMouseLeave={() => setHoveredIndex(null)}
+                >
+                    <a href={social.url} target="_blank" rel="noopener noreferrer" className='footer-container__link'>
+                        <Image src={social.photo} className='footer-container__link-photo'/>
+                    </a>
+
+                    {isAdmin && hoveredIndex === index && (
+                        <div className='group-socials-admin-buttons-container'>
+                            <Button
+                                className="group-socials-admin-buttons-container__edit"
+                            >
+                                <FaEdit/>
+                            </Button>
+                            <Button
+                                className="group-socials-admin-buttons-container__delete"
+                            >
+                                <FaTrash/>
+                            </Button>
+                        </div>
+                    )}
+                </div>
             ))}
         </div>
     );
