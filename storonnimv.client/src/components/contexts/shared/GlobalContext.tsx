c1ece7b9fc74e96247e6cc00bb2bf10ea54eeff5
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2025-05-10 23:50:36 +0300

    push

--- storonnimv.client/src/components/contexts/shared/GlobalContext.tsx
+++ storonnimv.client/src/components/contexts/shared/GlobalContext.tsx
@@ -44,7 +44,7 @@ const GlobalContextProvider: FC<GlobalContextProviderProps> = ({children}) => {
 
     const [validationErrors, setValidationErrors] = useState<Record<string, string[]>>({} as Record<string, string[]>);
 
-    const serverRoute = 'https://storonnimv-server.azurewebsites.net/api';
+    const serverRoute = 'https://storonnimv-h9ftbdb4hhd6eza4.northeurope-01.azurewebsites.net/api';
 
     const fetchIsAdmin = async () => {
         try {
