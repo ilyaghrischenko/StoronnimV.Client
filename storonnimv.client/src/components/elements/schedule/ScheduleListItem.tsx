47c1b0657b792e2d05e355341b9ceea76f5ea215
Sannido <dimabutnik71@gmail.com>
2025-05-13 20:20:43 +0300

    fixed styles and list rendering in SchedulesList.tsx

--- storonnimv.client/src/components/elements/schedule/ScheduleListItem.tsx
+++ storonnimv.client/src/components/elements/schedule/ScheduleListItem.tsx
@@ -1,15 +1,31 @@
-﻿import { FC } from "react";
-import { IScheduleListItem } from "../../../models/schedule/IScheduleListItem";
-import { Image } from "react-bootstrap";
+﻿import {FC, useContext} from "react";
+import {IScheduleListItem} from "../../../models/schedule/IScheduleListItem";
+import {Image} from "react-bootstrap";
+import {ScheduleContextProvider} from "../../contexts/ScheduleContext.tsx";
+import {ScheduleModal} from "./ScheduleModal.tsx";
+import {GlobalContext} from "../../contexts/shared/GlobalContext.tsx";
 
 interface ScheduleListItemProps {
     schedule: IScheduleListItem;
 }
 
-export const ScheduleListItem: FC<ScheduleListItemProps> = ({ schedule }) => {
+export const ScheduleListItem: FC<ScheduleListItemProps> = ({schedule}) => {
+    const globalContext = useContext(GlobalContext)!;
+
+    const {OnShowModal} = globalContext;
+
     return (
-        <div className='schedule-list-item'>
-            <Image className='schedule-list-item__photo' src={schedule.photo} fluid />
+        <div
+            className='schedule-list-item'
+            onClick={() =>
+                OnShowModal(
+                    <ScheduleContextProvider>
+                        <ScheduleModal scheduleId={schedule.id} />
+                    </ScheduleContextProvider>
+                )
+            }
+        >
+            <Image className='schedule-list-item__photo' src={schedule.photo} fluid/>
             {/*<div className='schedule-list-item__info'>*/}
             {/*    <p className='schedule-list-item__title'>{schedule.title}</p>*/}
             {/*    <p className='schedule-list-item__date'>{schedule.performanceDateTime}</p>*/}
--- storonnimv.client/src/components/elements/schedule/SchedulesList.tsx
+++ storonnimv.client/src/components/elements/schedule/SchedulesList.tsx
@@ -1,11 +1,7 @@
 ﻿import {FC, useContext, useEffect} from "react";
 import {ScheduleListItem} from "./ScheduleListItem";
-import {ScheduleContext, ScheduleContextProvider} from "../../contexts/ScheduleContext";
-import {List} from "../shared/GenericList/List";
-import {IScheduleListItem} from "../../../models/schedule/IScheduleListItem";
-import {ListItem} from "../shared/GenericList/ListItem";
+import {ScheduleContext} from "../../contexts/ScheduleContext";
 import {GlobalContext} from "../../contexts/shared/GlobalContext";
-import {ScheduleModal} from "./ScheduleModal.tsx";
 import {Button} from "react-bootstrap";
 import {PaginationSection} from "../shared/PaginationSection.tsx";
 import {AddScheduleModal} from "./forms/AddScheduleModal.tsx";
@@ -27,9 +23,9 @@ const SchedulesList: FC = () => {
         paginate(page);
     }, []);
 
-    // if (checkIfNoData(() => !schedules || schedules.length === 0)) {
-    //     return <NoData message='Афіш немає' />;
-    // }
+    if (checkIfNoData(() => !schedules || schedules.length === 0)) {
+        return <NoData message='Афіш немає' />;
+    }
 
     return (
         <div className='schedules-list'>
@@ -42,39 +38,21 @@ const SchedulesList: FC = () => {
                     </Button>
                 )}
 
-                {pageLoading ?
-                    <List
-                        className='schedules-list__grid'
-                        items={schedules}
-                        renderItem={(schedule: IScheduleListItem) => (
-                            <ListItem
-                                item={schedule}
+                <div className="schedules-list__items">
+                    {pageLoading
+                        ? Array(3).fill(null).map((_, i) => (
+                            <PreloaderTile
+                                key={i}
+                                className="preloader-tile__container-schedule-page"
+                            />
+                        ))
+                        : schedules.map((schedule) => (
+                            <ScheduleListItem
                                 key={schedule.id}
-                                renderItem={(schedule: IScheduleListItem) => (
-                                    <ScheduleListItem key={schedule.id} schedule={schedule}/>
-                                )}
-                                onClick={() =>
-                                    OnShowModal(
-                                        <ScheduleContextProvider>
-                                            <ScheduleModal scheduleId={schedule.id}/>
-                                        </ScheduleContextProvider>
-                                    )
-                                }
+                                schedule={schedule}
                             />
-                        )}
-                    />
-                    :
-                    <List
-                        className="schedules-list__items"
-                        items={Array(3).fill(null)}
-                        renderItem={(item: typeof PreloaderTile) => (
-                            <ListItem
-                                item={item}
-                                renderItem={() => <PreloaderTile className='preloader-tile__container-schedule-page'/>}
-                            />
-                        )}
-                    />
-                }
+                        ))}
+                </div>
             </div>
 
             <PaginationSection
