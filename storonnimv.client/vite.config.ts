998c939bfb4018b5946414e376cfef4fad92337a
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2026-01-31 18:27:26 +0200

    fix

--- storonnimv.client/vite.config.ts
+++ storonnimv.client/vite.config.ts
@@ -5,4 +5,8 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    host: true, // Позволяет доступ по IP
+    port: 5284, // (Опционально) Можно зафиксировать порт
+  },
 })
